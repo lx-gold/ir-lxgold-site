b82780b0b313d23c58dd5a89fa0e3d199d27d7d0
Mohammad Shamsi <mohamadshamsi76@gmail.com>
2023-11-03 21:23:00 +0330

    chain model

--- src/components/chain/select/select-panel/src/chain-select-panel-html.js
+++ src/components/chain/select/select-panel/src/chain-select-panel-html.js
@@ -69,7 +69,7 @@ module.exports = function buildSelectChainPanelHtml
                     }
 
                 return `
-                    <ul>
+                    <ul class="brand-medal-chain-model-list">
                         ${selectChainItemRenderHtmlResult}
                     </ul>
                 `;
